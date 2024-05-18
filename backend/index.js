4ef63b216cc384028b7493047752390bd8d38da0
saqibbedar <Chandio.Saqib.M@gmail.com>
2024-05-18 12:42:29 +0500

    update

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ const userModel = require("./Models/users")
 const app = express()
 
 app.use(cors({
-    origin:["https://login-system-frontend-lcj9ctbl0-saqib-bedars-projects.vercel.app"],
+    origin:["https://login-system-frontend-cfuvycoe4-saqib-bedars-projects.vercel.app"],
     methods: ['GET', 'POST', 'DELETE'],
     credential: true,
 }));
